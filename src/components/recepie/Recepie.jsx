66b57af724c3ce742e0791bf0b83f1252ead1bcb
endta <endertanver@gmail.com>
2022-07-09 22:55:10 +0100

    recepie

--- src/components/recepie/Recepie.jsx
+++ src/components/recepie/Recepie.jsx
@@ -5,13 +5,14 @@ import './recipe.css'
 const Recepie = () => {
     const apiID='175555ab'
     const apiKey="3650deaa6f6b5a9fc878b8e7a4bf46a2"
-    const [recipes, setRecipes] = React.useState([])
-    const [search, setSearch] = React.useState("")
+    const [recipes, setRecipes] = React.useState({})
+    const [search, setSearch] = React.useState("duck")
    
 
 
     useEffect(() => {
         getRecipes()
+        setSearch("")
     }, [])
 
 
@@ -21,8 +22,9 @@ const Recepie = () => {
         const response = await fetch(`https://api.edamam.com/search?q=${search}&app_id=${apiID}&app_key=${apiKey}`)
         
         const data = await response.json()
-        setRecipes(data.hits)
-        console.log(data.hits)
+       setRecipes(data.hits[0].recipe)
+        console.log(data.hits[0].recipe)   
+        
     }
 
     const searchHandler = (e) => {
@@ -34,25 +36,26 @@ const Recepie = () => {
 
   return (
     <div>
+        
         <h1>Recepie</h1>
         <input type="text" placeholder="Search" value={search} onChange={searchHandler} />
        
         <button onClick={getRecipes}>Search</button>
         <ul className='recepie'>
-            {recipes.map((recipe) => (
+            
                 <li>
 
     <div className="card-container">
     <div className="card u-clearfix">
       <div className="card-body">
         
-        <span className="card-author subtle">{recipe.recipe.source}</span>
-        <h2 className="card-title">{recipe.recipe.label}</h2>
+        <span className="card-author subtle">{recipes.source}</span>
+        <h2 className="card-title">{recipes.label}</h2>
         <span className="card-description subtle">These last few weeks I have been working hard on a new brunch recipe for you all.</span>
         <div className="card-read">Read</div>
        
       </div>
-      <img src={recipe.recipe.image} alt="" className="card-media" />
+      <img src={recipes.image} alt="" className="card-media" />
     </div>
     <div className="card-shadow" />
   </div>
@@ -60,8 +63,9 @@ const Recepie = () => {
                     
 
                 
-            ))}
+            
         </ul>
+
 
     </div>
   )
