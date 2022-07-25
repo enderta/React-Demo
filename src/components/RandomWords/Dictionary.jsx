1559807ccf2c68b53f5d920742edbc699abc6738
endta <endertanver@gmail.com>
2022-07-25 15:55:49 +0100

    dictionary

--- src/components/RandomWords/Dictionary.jsx
+++ src/components/RandomWords/Dictionary.jsx
@@ -3,18 +3,22 @@ import Card from 'react-bootstrap/Card'
 
 const Dictionary = (props) => {
   const [definition, setDefinition] = React.useState('')
-  const [randomword, setRandomword] = React.useState('')
   const [example, setExample] = React.useState('')
 
   
   React.useEffect(() => {
    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${props.word}`)
-    .then(response => response.json())
+    .then(response => response.json()
+    )
     .then(result => {
+      if (result.title==="No Definitions Found"){
+        setDefinition("No definition found")
+        setExample("No example found")
+      }else{
      setDefinition(result[0].meanings[0].definitions[0].definition)
       setExample(result[0].meanings[0].definitions[0].example)
       console.log(result[0].meanings[0].definitions)
-    }
+    }}
     , [])
   }, [props.word])
 
@@ -34,7 +38,8 @@ const Dictionary = (props) => {
       </Card>
       <Card style={{ width: '18rem' }}>
         <Card.Body>
-          <Card.Title>{definition}</Card.Title>
+          <Card.Title>
+          {definition}</Card.Title>
           <Card.Text>{example}</Card.Text>
         </Card.Body>
       </Card>
