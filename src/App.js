abad2adf73a962c96e6ef0338fd8680b66d2266c
endta <endertanver@gmail.com>
2022-07-28 14:08:02 +0100

    denem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react'
-import City from './components/Cities/City'
+import { Form, FormControl, Table } from 'react-bootstrap'
 
 const App = () => {
+  const contacts = [{
+    'id': 1,
+    'name': 'John Doe',
+    'phone': '555-555-5555',
+
+  }, {
+    'id': 2,
+    'name': 'Jane Doe',
+    'phone': '555-555-5555',
+  }
+  ]
+  const [search, setSearch] = React.useState('')
   return (
+
     <div>
-      <City   />
+     <Form >
+      <FormControl type="text" placeholder="Search" value={search} onChange={e => setSearch(e.target.value)} />
+     </Form>
+     <Table striped bordered hover>
+     <thead>
+      <tr>
+        <th>Name</th>
+        <th>Phone</th>
+      </tr>
+      </thead>
+      <tbody>
+      {
+        contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase())).map(contact => (
+          <tr key={contact.id}>
+            <td>{contact.name}</td>
+            <td>{contact.phone}</td>
+          </tr>
+        ))
+
+      }
+      </tbody>
+    </Table>
     </div>
   )
 }
