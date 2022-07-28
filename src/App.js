e2c4468dd2532f072a0197e4d499775ca1f264e6
endta <endertanver@gmail.com>
2022-07-28 16:18:05 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import React from 'react'
-import { Form, FormControl, Table } from 'react-bootstrap'
+import Cloud from './components/dataSecure/Cloud'
+import Data from './components/dataSecure/Data'
+import Footer from './components/dataSecure/Footer'
+import Hero from "./components/dataSecure/Hero"
+import Main from './components/dataSecure/Main'
+import Navbar from './components/dataSecure/Navbar'
 
 const App = () => {
-  const contacts = [{
-    'id': 1,
-    'name': 'John Doe',
-    'phone': '555-555-5555',
-
-  }, {
-    'id': 2,
-    'name': 'Jane Doe',
-    'phone': '555-555-5555',
-  }
-  ]
-  const [search, setSearch] = React.useState('')
   return (
-
     <div>
-     <Form >
-      <FormControl type="text" placeholder="Search" value={search} onChange={e => setSearch(e.target.value)} />
-     </Form>
-     <Table striped bordered hover>
-     <thead>
-      <tr>
-        <th>Name</th>
-        <th>Phone</th>
-      </tr>
-      </thead>
-      <tbody>
-      {
-        contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase())).map(contact => (
-          <tr key={contact.id}>
-            <td>{contact.name}</td>
-            <td>{contact.phone}</td>
-          </tr>
-        ))
-
-      }
-      </tbody>
-    </Table>
+ <Main />
     </div>
   )
 }
