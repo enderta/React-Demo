928d77794ba9aadcdb8678b4b87ff5ac1c6a4282
enderta <endertanver@gmail.com>
2022-08-03 15:21:03 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 const App = () => {
-  const [query, setQuery] = React.useState('')
-  const users=[
-    {id:1,first_name:'John',last_name:'Doe',email:'jd@mail.com',gender:'male'},
-    {id:2,first_name:'Jane',last_name:'Doe',email:'jane@mail.com',gender:'female'},
-    {id:3,first_name:'Fane',last_name:'Doe',email:'jane@mail.com',gender:'female'},
-    {id:4,first_name:'Lane',last_name:'Doe',email:'jane@mail.com',gender:'female'},
-    {id:5,first_name:'Sane',last_name:'Doe',email:'jane@mail.com',gender:'female'},
-    {id:6,first_name:'Dane',last_name:'Doe',email:'jane@mail.com',gender:'female'},
-    
+  const [users, setUsers] = useState([])
+  const [query, setQuery] = useState('')
 
-
-  ]
- 
   
+  
+  useEffect(() => {
+    fetch('https://random-data-api.com/api/users/random_user?size=3')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  console.log(users)
+
+
 
   return (
-    <div className='app'>
-    <input type="text" placeholder='Search...' className="search" onChange={(e)=>setQuery(e.target.value)} />
-   <table>
-    <thead>
-      <td>
-        ID
-      </td>
-      <td>
-        First Name
-      </td>
-      <td>
-        Last Name
-      </td>
-    
-    </thead>
-    <tbody>
-      {users.filter(user=>user.first_name.toLowerCase().includes(query.toLowerCase())
-      || user.last_name.toLowerCase().includes(query.toLowerCase)).map(user=>(
-        <tr key={user.id}>
-          <td>
-            {user.id}
-          </td>
-          <td>
-            {user.first_name}
-          </td>
-          <td>
-            {user.last_name}
-          </td>
-      </tr>
-      ))}
-    </tbody>
-   </table>
+    <div>
+      <input type="text" placeholder='search...' onChange={(e)=>setQuery(e.target.value)} />
+      <ul>
+        {
+          users.filter(user => user.first_name.toLowerCase().includes(query.toLowerCase()) || user.last_name.toLowerCase().includes(query.toLowerCase())).map(user => {
+            return <li>{user.first_name} {user.last_name}</li>
+          }
+          )
+        }
+
+      </ul>
+   
     </div>
   )
 }
+
+   
 
 export default App
