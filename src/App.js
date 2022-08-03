f4154d017dc75a28e1f855be45b0a1234c6b076a
enderta <endertanver@gmail.com>
2022-08-03 21:53:18 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,64 +1,78 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
+import { Table } from 'react-bootstrap'
 
 const App = () => {
   const [users, setUsers] = useState([])
   const [query, setQuery] = useState('')
   const [filteredUsers, setFilteredUsers] = useState(users)
+  const [isLoading, setIsLoading] = useState(false)
+const handleFilter=async()=>{
+  const response=await fetch(`https://random-data-api.com/api/users/random_user?size=10`)
+  const data=await response.json()
+  setUsers(data)
+  setFilteredUsers(data)
+}
+useEffect(()=>{
+  handleFilter()
+}
+,[])
 
-  
-  
-  useEffect(() => {
-    fetch('https://random-data-api.com/api/users/random_user?size=3')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  }, [])
-  console.log(users)
-
-  const handleFilter = (e) => {
-    setQuery(e.target.value)
-    const results = users.filter(user => user.first_name.toLowerCase().includes(e.target.value.toLowerCase()))
- setFilteredUsers(results) 
-    
-
+const handleQuery=(e)=>{
+  setQuery(e.target.value)
+  const filtered=users.filter(user=>{
+    return user.first_name.toLowerCase().includes(e.target.value.toLowerCase())
   }
+  )
+  setFilteredUsers(filtered)
+  
+}
+
+const handleLoading=()=>{
+  setIsLoading(true)
+  setTimeout(()=>{
+    setIsLoading(false)
+  }
+  ,2000)
+}
+
 
 
   return (
     <div>
-      <input type="text" value={query} onChange={handleFilter}/>
-   <table className="table table-striped">
-    <thead>
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Avatar</th>
-      </tr>
-    </thead>
-    <tbody>
-    {
-      query.length > 0 ? filteredUsers.map(user => {
-        return <tr>
+   <input style={{position:'fixed'}} type="text" placeholder='search...' value={query} onChange={handleQuery}  />
+   <br/>
+   <br/>
+   {
+      isLoading?<h1>Loading...</h1>:
+      <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>Avatar</th>
+    </tr>
+  </thead>
+  <tbody>
+    {filteredUsers.map((user,index)=>{
+      return(
+        <tr key={index}>
+          <td>{user.id}</td>
           <td>{user.first_name}</td>
           <td>{user.last_name}</td>
           <td>{user.email}</td>
           <td><img src={user.avatar} alt="avatar"/></td>
         </tr>
-      }
-      ) : users.map(user => {
-        return <tr>
-          <td>{user.first_name}</td>
-          <td>{user.last_name}</td>
-          <td>{user.email}</td>
-          <td><img src={user.avatar} alt="avatar"/></td>
-        </tr>
-      }
       )
     }
-    </tbody>
-  </table>
-   
+    )}
+  </tbody>
+</Table>
+   }
+
+
     </div>
   )
 }
