955e51cf3a6c4c638e31b7bd808ec06f03b2e4cc
enderta <endertanver@gmail.com>
2022-08-09 14:46:31 +0100

    mui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import { Container, Grid } from '@mui/material'
+import { Container, Grid,Paper } from '@mui/material'
 import React from 'react'
 import TourCard from './components/materialUI/TourCard'
 function App() {
   return (
     <div>
-      <Container>
+    
+    <TourCard/>
+   
 
-        <Grid container spacing={3}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>  
-      </Container>
-    </div>
+            </div>
   )
 }
 
--- src/components/materialUI/TourCard.jsx
+++ src/components/materialUI/TourCard.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
-import { Container,Paper,Grid} from '@mui/material'
+import { Container,Paper,Grid,CssBaseline, AppBar,Typography,Toolbar} from '@mui/material'
+import { PhotoCamera } from '@mui/icons-material'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
+
 
 const TourCard = () => {
   return (
     <div>
-        <Grid item xs={3}>
-            <Paper elevation={5}>
-               hello
-            </Paper>
-        </Grid>
+        <CssBaseline />
+        <AppBar position="relative" color="primary">
+          <Toolbar>
+           <PhotoCamera />
+           <Typography variant="h6" color="inherit" noWrap>
+              Tour Card
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+        <div>
+        <Container maxWidth="sm">
+          <Typography variant="h6" gutterBottom color="textPrimary" align="center">
+            Tour Card
+          </Typography>
+          <Typography variant="body1" gutterBottom color="textPrimary" align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+            Cras ultricies ligula sed magna dictum porta.
+            Curabitur aliquet quam id dui posuere blandit.
+            </Typography>
+            </Container>
+        </div>
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>Button 1</Button>
+          <Button>Button 2</Button>
+          <Button>Button 3</Button>
+        </ButtonGroup>
+        </main>
+         
+
 
     </div>
   )
